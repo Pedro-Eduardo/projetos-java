f0e68aff5a904d6889e840341909ed3137ef63c1
pedro-eduardo <pedroeduardopereiradasilva@gmail.com>
2022-05-03 20:57:42 -0300

    testando ambiente para ciração de projeto da dio week santander

---
+++ dio/santander-universidade/dio-santander-universidade/aula-java/src/dio/auladejava/MinhaClasse.java
@@ -0,0 +1,7 @@
+package dio.auladejava;
+
+public class MinhaClasse {
+	public static void main(String[] args) {
+		System.out.println("Olá, Planeta da Programação!!  Vocês São de Outro Mundo :P");
+	}
+}
